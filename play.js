1101de57859c1a70595f96111249473ca8c0d369
Preetirani123 <rani.preeti3333@gmail.com>
2021-02-10 20:50:51 +0000

    add play.js file

---
+++ play.js
@@ -0,0 +1,29 @@
+const net = require('net');
+
+/**
+ * Establishes connection with the game server
+ */
+const connect = function(data) {
+  const conn = net.createConnection({ 
+    host: '135.23.222.131',
+    port: 50542
+  });
+  // interpret incoming data as text
+  conn.setEncoding('utf8'); 
+  conn.on('connecting', () => {
+    conn.write(data);
+  })
+  conn.on('data', (data) => {
+    console.log('New message from the server', data)
+  })
+
+  return conn;
+}
+
+
+
+
+console.log('Connecting here');
+connect();
+
+
